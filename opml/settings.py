31198080c116a46f5ed5f372403601b657aea285
Eric Davis <eric@davising.com>
2014-01-12 16:14:16 -0800

    settings.py: disable DEBUG on production

--- opml/settings.py
+++ opml/settings.py
@@ -21,11 +21,10 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', 'development')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'e+t11fi-hrjy$ja^i^1hg0752mho)r*xm*kir$u5)su%u7+d&k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (ENVIRONMENT == 'development')
+TEMPLATE_DEBUG = DEBUG
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['opml.rsshub.org']
 
 
 # Application definition
